6a8c607d08d96dd90b46e4873652998528861aaf
Snku1 <andpra10705@gmail.com>
2025-11-16 01:27:13 +0700

    perbarui untuk deploy

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) # Hanya untuk development
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
